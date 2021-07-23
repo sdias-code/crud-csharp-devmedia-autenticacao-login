4ec7c9b6339a5014a7788d6b70cce86f495c1089
sdias-code <silviodias.ms@gmail.com>
2021-07-23 11:04:23 -0400

    first commit

---
+++ autenticacaologin/AutenticacaoLogin/AutenticacaoLogin/Controllers/AutenticacaoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutenticacaoLogin.Controllers
+{
+    public class AutenticacaoController : Controller
+    {
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+    }
+}

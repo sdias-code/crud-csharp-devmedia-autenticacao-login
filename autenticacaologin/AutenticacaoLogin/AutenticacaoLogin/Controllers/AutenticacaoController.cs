957de6b819bfb6e8b12ceb51c80ea1f8d000cdd7
sdias-code <silviodias.ms@gmail.com>
2021-07-23 11:30:57 -0400

    Metodo Create funcionando

--- autenticacaologin/AutenticacaoLogin/AutenticacaoLogin/Controllers/AutenticacaoController.cs
+++ autenticacaologin/AutenticacaoLogin/AutenticacaoLogin/Controllers/AutenticacaoController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutenticacaoLogin.Models;
+using AutenticacaoLogin.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,31 @@ namespace AutenticacaoLogin.Controllers
 {
     public class AutenticacaoController : Controller
     {
+        private UsuariosContext db = new UsuariosContext();
         public ActionResult Cadastrar()
         {
             return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Cadastrar(CadastroUsuarioViewModels viewmodel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(viewmodel);
+            }
+
+            Usuario novoUsuario = new Usuario
+            {
+                Nome = viewmodel.Nome,
+                Login = viewmodel.Login,
+                Senha = viewmodel.Senha
+            };
+
+            db.Usuarios.Add(novoUsuario);
+            db.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
